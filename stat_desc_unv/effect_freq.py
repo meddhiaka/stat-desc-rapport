65e11310f983de621ca57ffe75ec714434d474da
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-11-29 19:48:54 +0100

    code: effectufs & frequences functions added

---
+++ stat_desc_unv/effect_freq.py
@@ -0,0 +1,20 @@
+def effectifs(l):
+    res= [] # liste contenant l'effectif de chaque élement
+    s= 0 # taille totale d'effectifs
+    for e in l:
+        res.append({e: l.count(e)})
+    for e in res:
+        s += list(e.items())[0][1] # l'élement selectionné est l'effectif
+    return res, s
+
+def frequences(l, s):
+    res= [] # liste contenant la fréquence de chaque élement
+    for e in l:
+        res.append({list(e.items())[0][0]: list(e.items())[0][1]/s*100})
+    return res
+    
+
+
+    
+    
+
