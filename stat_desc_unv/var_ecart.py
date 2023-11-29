4200e5b48d3df04ee8856da559a3539da12add45
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-11-29 22:20:27 +0100

    code: variance & ecart type funcs added

---
+++ stat_desc_unv/var_ecart.py
@@ -0,0 +1,16 @@
+def variance(l: list[int]):
+    s1 = 0
+    s2 = 0
+    i = 0
+    for e in l:
+        s1 += e
+    xb = s1 / len(l)
+    while i < len(l):
+        s2 += (l[i] - xb) ** 2
+        i +=1
+    s2 = s2 / len(l)
+    return s2
+
+def ecartType(e: float):
+    return variance(e) ** 0.5
+
