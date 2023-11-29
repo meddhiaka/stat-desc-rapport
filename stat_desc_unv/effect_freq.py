d3689d0bcebda8a055251d6d829aa7220cbc75be
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-11-29 22:22:10 +0100

    code: adding type annotations to effect_freq module

--- stat_desc_unv/effect_freq.py
+++ stat_desc_unv/effect_freq.py
@@ -1,4 +1,4 @@
-def effectifs(l):
+def effectifs(l: list[int]):
     res= [] # liste contenant l'effectif de chaque élement
     s= 0 # taille totale d'effectifs
     for e in l:
@@ -7,7 +7,7 @@ def effectifs(l):
         s += list(e.items())[0][1] # l'élement selectionné est l'effectif
     return res, s
 
-def frequences(l, s):
+def frequences(l: list[dict[int, int]], s:int):
     res= [] # liste contenant la fréquence de chaque élement
     for e in l:
         res.append({list(e.items())[0][0]: list(e.items())[0][1]/s*100})
