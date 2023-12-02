fc7cdac39c0798a59e8ab7c6e2a7306e247b8bc1
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-12-02 14:02:00 +0100

    code: first part automation is completed

--- tasks.py
+++ tasks.py
@@ -3,3 +3,8 @@ from invoke import task
 @task
 def run(e):
     e.run("python main.py")
+
+@task
+def run_partie1(e):
+    # Write score instead of underscore in the terminal while executing tasks :)
+    e.run("python -m stat_desc_unv")
